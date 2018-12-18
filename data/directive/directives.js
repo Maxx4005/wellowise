43f2a9d1192b8793817c2c28605555d531b9d820
Hemant <hemant@diligencevault.com>
2018-12-19 01:28:11 +0530

    angularjs integration

--- data/directive/directives.js
+++ data/directive/directives.js
@@ -1,9 +1,23 @@
 testApp.directive('navBar', function () {
   return {
-    restrict: 'E',
+    restrict: 'EA',
     templateUrl: 'data/directive/header.html'
   };
 });
+testApp.directive('appFooter', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: 'data/directive/footer.html'
+  };
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
 testApp.directive('ngEnter', function () {
     return function (scope, element, attrs) {
